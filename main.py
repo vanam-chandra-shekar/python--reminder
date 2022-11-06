c10a0b3026fa1bbf56a2ba14e610b3a9fbc03241
vanam-chandra-shekar <vanamchandrashekar@outlook.com>
2022-11-06 23:34:45 +0530

    modularized the code

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from PyDictionary import PyDictionary
 from plyer import notification
 import random
+import time
 
 
 file = open('words.txt','r')
 words = file.read().split(',')
 file.close()
-rand_word = words[random.randrange(0,len(words)-1)].strip('\n')
 
+def nofify():
+    rand_word = words[random.randrange(0,len(words)-1)].strip('\n')
+    dictionary = PyDictionary()
+    keys = list(dictionary.meaning(rand_word).keys())
+    meaning = dictionary.meaning(rand_word)[keys[0]][0]
 
-dictionary = PyDictionary()
-keys = list(PyDictionary.meaning(rand_word).keys())
-meaning = PyDictionary.meaning(rand_word)[keys[0]][0]
+    notification.notify(
+        title = rand_word,
+            message = meaning,
+            app_name = 'reminder',
+            timeout = 6
+    )
+    time.sleep(10)
 
-notification.notify(
-    title = rand_word,
-    message = meaning,
-    app_name = 'reminder',
-    timeout = 6
-)
+while True:
+    nofify()
